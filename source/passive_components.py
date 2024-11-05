4e86ec28ba096498adb7e4bad3ceb88b95a84e6d
Eduardo Heredia Gonzalez <EduardoHerediaGonzalez@gmail.com>
2024-11-05 12:20:40 -0600

    Code refactor on all the classes into the file .

--- source/passive_components.py
+++ source/passive_components.py
@@ -3,187 +3,162 @@ import numpy as np
 # Definition of the class that represent a resistor component
 class Resistor:
     # Private class attributes
-    __impedance_value: complex
-    __admittance_value: complex
+    __resistance: float = 0
+    __conductance: float = 0
 
     # Class constructors
-    def __init__(self, resistance_value: float):
-        self.__resistance_value = resistance_value
-        self.__conductance_value = 1 / resistance_value
+    def __init__(self, resistance: float = 0):
+        self.__resistance = resistance
+        try:
+            self.__conductance = 1 / resistance
+        except:
+            self.__conductance = 0
 
     # Public class methods
-    def set_resistance(self, resistance_value):
-        self.__resistance_value = resistance_value
-        self.__conductance_value = 1 / resistance_value
+    def set_resistance(self, to_value: float):
+        self.__resistance = to_value
+        self.__conductance = 1 / to_value
 
     def get_resistance(self):
-        return self.__resistance_value
+        return self.__resistance
 
     def get_conductance(self):
-        return self.__conductance_value
-
-    def get_impedance(self):
-        reactance = 0
-        self.__impedance_value = complex(self.__resistance_value, reactance)
-
-        return  self.__impedance_value
-
-    def get_admittance(self):
-        reactance = 0
-        self.__admittance_value = complex(self.__conductance_value, reactance)
-
-        return self.__admittance_value
+        return self.__conductance
 
 
 # Definition of the class that represent a capacitor component
 class Capacitor:
     # Private class attributes
-    __reactance_value: float
-    __susceptance_value: float
-    __impedance_value: complex
-    __admittance_value: complex
+    __capacitance: float = 0
+    __reactance: float = 0
+    __susceptance: float = 0
 
     # Class constructors
-    def __init__(self, capacitance_value: float):
-        self.__capacitance_value = capacitance_value
-
-    # Private class methods
+    def __init__(self, capacitance: float = 0):
+        self.__capacitance = capacitance
 
     # Public class methods
-    def get_reactance(self, at_frequency: int):
-        self.__reactance_value = -1 / (2 * np.pi * at_frequency * self.__capacitance_value)
+    def set_capacitance(self, to_value: float):
+        self.__capacitance = to_value
 
-        return self.__reactance_value
+    def get_capacitance(self):
+        return self.__capacitance
 
-    def get_susceptance(self, at_frequency: int):
-        self.__susceptance_value = 2 * np.pi * at_frequency * self.__capacitance_value
+    def get_reactance(self, at_frequency: float):
+        try:
+            self.__reactance = -1 / (2 * np.pi * at_frequency * self.__capacitance)
+        except:
+            self.__reactance = float(0)
 
-        return self.__susceptance_value
+        return self.__reactance
 
-    def get_impedance(self, at_frequency):
-        resistance = 0
-        reactance = self.get_reactance(at_frequency=at_frequency)
+    def get_susceptance(self, at_frequency: float):
+        self.__susceptance = 2 * np.pi * at_frequency * self.__capacitance
 
-        self.__impedance_value = complex(resistance, reactance)
-
-        return self.__impedance_value
-
-    def get_admittance(self, at_frequency):
-        conductance = 0
-        susceptance = self.get_susceptance(at_frequency=at_frequency)
-
-        self.__admittance_value = complex(conductance, susceptance)
-
-        return self.__admittance_value
+        return self.__susceptance
 
 
 # Definition of the class that represent an inductor component
 class Inductor:
     # Private class attributes
-    __reactance_value: float
-    __susceptance_value: float
-    __impedance_value: complex
-    __admittance_value: complex
+    __inductance: float = 0
+    __reactance: float = 0
+    __susceptance: float = 0
 
     # Class constructors
-    def __init__(self, inductance_value: float):
-        self.__inductance_value = inductance_value
-
-    # Private class methods
+    def __init__(self, inductance: float = 0):
+        self.__inductance = inductance
 
     # Public class methods
-    def get_reactance(self, at_frequency: int):
-        self.__reactance_value = 2 * np.pi * at_frequency * self.__inductance_value
+    def set_inductance(self, to_value):
+        self.__inductance = to_value
 
-        return self.__reactance_value
+    def get_inductance(self):
+        return self.__inductance
 
-    def get_susceptance(self, at_frequency: int):
-        self.__susceptance_value = -1 / (2 * np.pi * at_frequency * self.__inductance_value)
+    def get_reactance(self, at_frequency: float):
+        self.__reactance = 2 * np.pi * at_frequency * self.__inductance
 
-        return  self.__susceptance_value
+        return self.__reactance
 
-    def get_impedance(self, at_frequency):
-        resistance = 0
-        reactance = self.get_reactance(at_frequency=at_frequency)
+    def get_susceptance(self, at_frequency: float):
+        try:
+            self.__susceptance = -1 / (2 * np.pi * at_frequency * self.__inductance)
+        except:
+            self.__susceptance = float(0)
 
-        self.__impedance_value = complex(resistance, reactance)
-
-        return self.__impedance_value
-
-    def get_admittance(self, at_frequency):
-        conductance = 0
-        susceptance = self.get_susceptance(at_frequency=at_frequency)
-
-        self.__admittance_value = complex(conductance, susceptance)
-
-        return self.__admittance_value
+        return  self.__susceptance
 
 
 # Definition of the class that represent an impedance.
 class Impedance:
     # Private class attributes
-    __operation_frequency: int
     __total_impedance: complex
+    __type_of_element: str = ''
+    __element_value: float = 0
+    __resistance: float = 0
+    __reactance: float = 0
+    __impedance: complex = complex(0,0)
 
     # Class constructors
-    def __init__(self, type_of_element: str = '', with_value: float = 1):
+    def __init__(self, type_of_element: str = '', with_value: float = 0):
         self.__type_of_element = type_of_element
         self.__element_value = with_value
 
-    # Private class methods
-
     # Public class methods
     def get_impedance(self, at_frequency):
-        self.__operation_frequency = at_frequency
-
         if self.__type_of_element == 'Resistor':
-            resistor = Resistor(resistance_value=self.__element_value)
-            self.__total_impedance = resistor.get_impedance()
+            self.__resistance = Resistor(resistance=self.__element_value).get_resistance()
+            self.__reactance = float(0)
 
         elif self.__type_of_element == 'Capacitor':
-            capacitor = Capacitor(capacitance_value=self.__element_value)
-            self.__total_impedance = capacitor.get_impedance(at_frequency=self.__operation_frequency)
+            self.__resistance = float(0)
+            self.__reactance = Capacitor(capacitance=self.__element_value).get_reactance(at_frequency=at_frequency)
 
         elif self.__type_of_element == 'Inductor':
-            inductor = Inductor(inductance_value=self.__element_value)
-            self.__total_impedance = inductor.get_impedance(at_frequency=self.__operation_frequency)
+            self.__resistance = float(0)
+            self.__reactance = Inductor(inductance=self.__element_value).get_reactance(at_frequency=at_frequency)
 
         else:
-            self.__total_impedance = complex(0,0)
+            self.__impedance = complex(0,0)
 
-        return self.__total_impedance
+        self.__impedance = complex(self.__resistance, self.__reactance)
+
+        return self.__impedance
 
 
 # Definition of the class that represent an admittance.
 class Admittance:
     # Class attributes
-    __operation_frequency: int
-    __total_admittance: complex
+    __total_impedance: complex
+    __type_of_element: str = ''
+    __element_value: float = 0
+    __conductance: float = 0
+    __susceptance: float = 0
+    __admittance: complex = complex(0,0)
 
     # Class constructors
-    def __init__(self, type_of_element: str = '', with_value: float = 1):
+    def __init__(self, type_of_element: str = '', with_value: float = 0):
         self.__type_of_element = type_of_element
         self.__element_value = with_value
 
-    # Private class methods
-
     # Public class methods
     def get_admittance(self, at_frequency):
-        self.__operation_frequency = at_frequency
-
         if self.__type_of_element == 'Resistor':
-            resistor = Resistor(resistance_value=self.__element_value)
-            self.__total_admittance = resistor.get_admittance()
+            self.__conductance = Resistor(resistance=self.__element_value).get_conductance()
+            self.__susceptance = float(0)
 
         elif self.__type_of_element == 'Capacitor':
-            capacitor = Capacitor(capacitance_value=self.__element_value)
-            self.__total_admittance = capacitor.get_admittance(at_frequency=self.__operation_frequency)
+            self.__conductance = float(0)
+            self.__susceptance = Capacitor(capacitance=self.__element_value).get_susceptance(at_frequency=at_frequency)
 
         elif self.__type_of_element == 'Inductor':
-            inductor = Inductor(inductance_value=self.__element_value)
-            self.__total_admittance = inductor.get_admittance(at_frequency=self.__operation_frequency)
+            self.__conductance = float(0)
+            self.__susceptance = Inductor(inductance=self.__element_value).get_susceptance(at_frequency=at_frequency)
 
         else:
-            self.__total_admittance = complex(0,0)
+            self.__admittance = complex(0,0)
 
-        return self.__total_admittance
+        self.__admittance = complex(self.__conductance, self.__susceptance)
+
+        return self.__admittance
