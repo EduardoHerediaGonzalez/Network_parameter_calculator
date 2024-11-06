b3cbd13cffa8726ace4fd40c37ce1b38e9b8f600
Eduardo Heredia Gonzalez <EduardoHerediaGonzalez@gmail.com>
2024-11-05 19:45:55 -0600

    Code refactor on classes SeriesImpedanceCircuit, ShuntImpedanceCircuit and TCircuit.

--- source/common_two_port_circuits.py
+++ source/common_two_port_circuits.py
@@ -15,29 +15,14 @@ class SeriesImpedanceCircuit:
     def __init__(self, type_of_element : str = '', element_value : float = 0):
         self.impedance = Impedance(type_of_element=type_of_element, with_value=element_value)
 
-    # Private class methods
-    def __get_parameter_a(self):
-        return self.__parameter_a
-
-    def __get_parameter_b(self, at_frequency):
-        self.__parameter_b = self.impedance.get_impedance(at_frequency=at_frequency)
-
-        return self.__parameter_b
-
-    def __get_parameter_c(self):
-        return self.__parameter_c
-
-    def __get_parameter_d(self):
-        return self.__parameter_d
-
     # Public class methods
     def get_delta_abcd(self, at_frequency):
-        self.__delta_abcd = (self.__parameter_a * self.__parameter_d) - (self.__get_parameter_b(at_frequency=at_frequency) * self.__parameter_c)
+        self.__delta_abcd = (self.__parameter_a * self.__parameter_d) - (self.impedance.get_impedance(at_frequency=at_frequency) * self.__parameter_c)
 
-        return  self.__delta_abcd
+        return  int(self.__delta_abcd.real)
 
     def get_matrix_abcd(self, at_frequency):
-        self.__matrix_abcd = np.matrix([[self.__parameter_a, self.__get_parameter_b(at_frequency=at_frequency)] , [self.__parameter_c, self.__parameter_d]], dtype=complex)
+        self.__matrix_abcd = np.matrix([[self.__parameter_a, self.impedance.get_impedance(at_frequency=at_frequency)],[self.__parameter_c, self.__parameter_d]])
 
         return self.__matrix_abcd
 
@@ -55,28 +40,14 @@ class ShuntImpedanceCircuit:
     def __init__(self, type_of_element : str = '', element_value : float = 0):
         self.admittance = Admittance(type_of_element=type_of_element, with_value=element_value)
 
-    # Private class methods
-    def __get_parameter_a(self):
-        return self.__parameter_a
-
-    def __get_parameter_b(self):
-        return self.__parameter_b
-
-    def __get_parameter_c(self, at_frecuency):
-        self.__parameter_c = self.admittance.get_admittance(at_frequency=at_frecuency)
-        return self.__parameter_c
-
-    def __get_parameter_d(self):
-        return self.__parameter_d
-
     # Public class methods
     def get_delta_abcd(self, at_frequency):
-        self.__delta_abcd = (self.__parameter_a * self.__parameter_d) - (self.__parameter_b * self.__get_parameter_c(at_frecuency=at_frequency))
+        self.__delta_abcd = (self.__parameter_a * self.__parameter_d) - (self.__parameter_b * self.admittance.get_admittance(at_frequency=at_frequency))
 
-        return  self.__delta_abcd
+        return  int(self.__delta_abcd.real)
 
     def get_matrix_abcd(self, at_frequency):
-        self.__matrix_abcd = np.matrix([[self.__parameter_a, self.__parameter_b] , [self.__get_parameter_c(at_frecuency=at_frequency), self.__parameter_d]], dtype=complex)
+        self.__matrix_abcd = np.matrix([[self.__parameter_a, self.__parameter_b],[self.admittance.get_admittance(at_frequency=at_frequency), self.__parameter_d]])
 
         return self.__matrix_abcd
 
@@ -103,44 +74,55 @@ class TCircuit:
 
     # Private class methods
     def __get_parameter_a(self, at_frequency):
-        self.__z_a = self.impedance_a.get_impedance(at_frequency=at_frequency)
-        self.__z_c = self.impedance_c.get_impedance(at_frequency=at_frequency)
+        impedance_z_a = self.impedance_a.get_impedance(at_frequency=at_frequency)
+        impedance_z_c = self.impedance_c.get_impedance(at_frequency=at_frequency)
 
-        self.__parameter_a = 1 + (self.__z_a / self.__z_c)
+        self.__parameter_a = 1 + (impedance_z_a / impedance_z_c)
 
         return self.__parameter_a
 
     def __get_parameter_b(self, at_frequency):
-        self.__z_a = self.impedance_a.get_impedance(at_frequency=at_frequency)
-        self.__z_b = self.impedance_b.get_impedance(at_frequency=at_frequency)
-        self.__z_c = self.impedance_c.get_impedance(at_frequency=at_frequency)
+        impedance_z_a = self.impedance_a.get_impedance(at_frequency=at_frequency)
+        impedance_z_b = self.impedance_b.get_impedance(at_frequency=at_frequency)
+        impedance_z_c = self.impedance_c.get_impedance(at_frequency=at_frequency)
 
-        self.__parameter_b = self.__z_a + self.__z_b + ((self.__z_a * self.__z_b) / self.__z_c)
+        self.__parameter_b = impedance_z_a + impedance_z_b + ((impedance_z_a * impedance_z_b) / impedance_z_c)
 
         return self.__parameter_b
 
     def __get_parameter_c(self, at_frequency):
-        self.__z_c = self.impedance_c.get_impedance(at_frequency=at_frequency)
-        self.__parameter_c = 1 / self.__z_c
+        impedance_z_c = self.impedance_c.get_impedance(at_frequency=at_frequency)
+
+        self.__parameter_c = 1 / impedance_z_c
 
         return self.__parameter_c
 
     def __get_parameter_d(self, at_frequency):
-        self.__z_b = self.impedance_b.get_impedance(at_frequency=at_frequency)
-        self.__z_c = self.impedance_c.get_impedance(at_frequency=at_frequency)
+        impedance_z_b = self.impedance_b.get_impedance(at_frequency=at_frequency)
+        impedance_z_c = self.impedance_c.get_impedance(at_frequency=at_frequency)
 
-        self.__parameter_d = 1 + (self.__z_b / self.__z_c)
+        self.__parameter_d = 1 + (impedance_z_b / impedance_z_c)
 
         return self.__parameter_d
 
     # Public class methods
     def get_delta_abcd(self, at_frequency):
-        self.__delta_abcd = (self.__get_parameter_a(at_frequency=at_frequency) * self.__get_parameter_d(at_frequency=at_frequency)) - (self.__get_parameter_b(at_frequency=at_frequency) * self.__get_parameter_c(at_frequency=at_frequency))
+        parameter_a = self.__get_parameter_a(at_frequency=at_frequency)
+        parameter_b = self.__get_parameter_b(at_frequency=at_frequency)
+        parameter_c = self.__get_parameter_c(at_frequency=at_frequency)
+        parameter_d = self.__get_parameter_d(at_frequency=at_frequency)
+
+        self.__delta_abcd = (parameter_a * parameter_d) - (parameter_b * parameter_c)
 
         return  self.__delta_abcd
 
     def get_matrix_abcd(self, at_frequency):
-        self.__matrix_abcd = np.matrix([[self.__get_parameter_a(at_frequency=at_frequency), self.__get_parameter_b(at_frequency=at_frequency)] , [self.__get_parameter_c(at_frequency=at_frequency), self.__get_parameter_d(at_frequency=at_frequency)]], dtype=complex)
+        parameter_a = self.__get_parameter_a(at_frequency=at_frequency)
+        parameter_b = self.__get_parameter_b(at_frequency=at_frequency)
+        parameter_c = self.__get_parameter_c(at_frequency=at_frequency)
+        parameter_d = self.__get_parameter_d(at_frequency=at_frequency)
+
+        self.__matrix_abcd = np.matrix([[parameter_a, parameter_b],[parameter_c, parameter_d]])
 
         return self.__matrix_abcd
 
