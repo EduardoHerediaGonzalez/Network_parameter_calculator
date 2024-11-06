6e5747fadf931f42c1eb57a2fbcffbb03275e16c
Eduardo Heredia Gonzalez <EduardoHerediaGonzalez@gmail.com>
2024-11-05 19:01:11 -0600

    Implementation of functions that convert parameters ABCD to Z and Y.

---
+++ source/network_parameter_conversions.py
@@ -0,0 +1,38 @@
+import numpy as np
+from numpy.matrixlib.defmatrix import matrix
+
+def convert_abcd_parameters_to_z_parameters(matrix_abcd: matrix, delta: complex):
+    matrix_abcd = matrix_abcd.tolist()
+    matrix_abcd_row_1 = matrix_abcd[0]
+    matrix_abcd_row_2 = matrix_abcd[1]
+
+    parameter_a = matrix_abcd_row_1[0]
+    parameter_c = matrix_abcd_row_2[0]
+    parameter_d = matrix_abcd_row_2[1]
+
+    parameter_z11 = parameter_a / parameter_c
+    parameter_z12 = delta / parameter_c
+    parameter_z21 = 1 / parameter_c
+    parameter_z22 = parameter_d / parameter_c
+
+    z_parameters_matrix = np.matrix[[parameter_z11, parameter_z12],[parameter_z21, parameter_z22]]
+
+    return z_parameters_matrix
+
+def convert_abcd_parameters_to_y_parameters(matrix_abcd: matrix, delta: complex):
+    matrix_abcd = matrix_abcd.tolist()
+    matrix_abcd_row_1 = matrix_abcd[0]
+    matrix_abcd_row_2 = matrix_abcd[1]
+
+    parameter_a = matrix_abcd_row_1[0]
+    parameter_b = matrix_abcd_row_1[1]
+    parameter_d = matrix_abcd_row_2[1]
+
+    parameter_y11 = parameter_d / parameter_b
+    parameter_y12 = -delta / parameter_b
+    parameter_y21 = -1 / parameter_b
+    parameter_y22 = parameter_a / parameter_d
+
+    y_parameters_matrix = np.matrix[[parameter_y11, parameter_y12], [parameter_y21, parameter_y22]]
+
+    return y_parameters_matrix
